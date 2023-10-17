36b17a400d8465e50cc45e5c0c83914feffdbed7
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2023-10-17 21:28:21 +0200

    Bugfixes

--- modxom.c
+++ modxom.c
@@ -13,21 +13,22 @@
 #include <linux/mm.h>
 #include <linux/list.h>
 
-#define MIN(X, Y)               ((X) < (Y) ? (X) : (Y))
-#define MODXOM_PROC_FILE_NAME   "xom"
-#define READ_HEADER_STRING      "        Address:             Size:      File-Offset:\n"
-#define MAPPING_LINE_SIZE       ((3 * (2 * sizeof(size_t) + 2)) + 5)
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+#define MODXOM_PROC_FILE_NAME "xom"
+#define READ_HEADER_STRING "        Address:             Size:\n"
+#define MAPPING_LINE_SIZE ((2 * (2 * sizeof(size_t) + 2)) + 5)
 
-struct {
+struct
+{
     struct list_head lhead;
-    loff_t mmap_offset;
     unsigned int num_pages;
     unsigned int num_refs;
     unsigned long kaddr;
     unsigned long uaddr;
 } typedef xom_mapping, *pxom_mapping;
 
-struct {
+struct
+{
     struct list_head lhead;
     pid_t pid;
     struct list_head mappings;
@@ -35,82 +36,73 @@ struct {
 
 LIST_HEAD(xom_entries);
 
-static int release_mapping(pxom_mapping mapping) {
-    unsigned long i, vm_flags;
-    struct vm_area_struct* vma;
-    struct page* page = virt_to_page(mapping->kaddr);
+static int release_mapping(pxom_mapping mapping)
+{
+    unsigned long i;
+    struct vm_area_struct *vma;
+    struct page *page = virt_to_page(mapping->kaddr);
 
-    if(!page)
+    if (!page)
         return -EFAULT;
 
     for (i = 0; i < mapping->num_pages * PAGE_SIZE; i += PAGE_SIZE)
         ClearPageReserved(virt_to_page(mapping->kaddr + i));
 
     // Don't mess with a dying processes address space
-    if(!(current->flags & PF_EXITING)){
+    if (!(current->flags & PF_EXITING))
+    {
         vma = find_vma(current->mm, mapping->uaddr);
-        vm_flags = vma->vm_flags | VM_DONTEXPAND | VM_DONTDUMP;
-        if (remap_pfn_range(vma, vma->vm_start, 0, mapping->num_pages * PAGE_SIZE, (pgprot_t){vm_flags})) {
-            printk(KERN_ERR "Failed to unmap pages from user space\n");
+        if(vm_munmap(vma->vm_start, mapping->num_pages * PAGE_SIZE))
             return -EFAULT;
-        }
     }
-    
-    for (i = 0; i < mapping->num_pages * PAGE_SIZE; i += PAGE_SIZE)
-        __free_page(virt_to_page(mapping->kaddr + i));
+
+    free_pages(mapping->kaddr, get_order(mapping->num_pages * PAGE_SIZE));
     return 0;
 }
 
-static pxom_process_entry get_process_entry(void){
-    pxom_process_entry curr_entry = (pxom_process_entry) xom_entries.next;
-    while ((void*)curr_entry != &xom_entries) {
+static pxom_process_entry get_process_entry(void)
+{
+    pxom_process_entry curr_entry = (pxom_process_entry)xom_entries.next;
+    while ((void *)curr_entry != &xom_entries)
+    {
         if (curr_entry->pid == current->pid)
             break;
-        curr_entry = (pxom_process_entry) curr_entry->lhead.next;
+        curr_entry = (pxom_process_entry)curr_entry->lhead.next;
     }
-    if ((void*)curr_entry == &xom_entries)
+    if ((void *)curr_entry == &xom_entries)
         return NULL;
-    return curr_entry; 
+    return curr_entry;
 }
 
-static int release_process(pxom_process_entry curr_entry){
+static int release_process(pxom_process_entry curr_entry)
+{
     pxom_mapping last_mapping, curr_mapping;
 
     if (!curr_entry)
         return -EINVAL;
-    
-    curr_mapping = (pxom_mapping) curr_entry->mappings.next;
 
-    while ((void*)curr_mapping != &(curr_entry->mappings)) {
+    curr_mapping = (pxom_mapping)curr_entry->mappings.next;
+
+    while ((void *)curr_mapping != &(curr_entry->mappings))
+    {
         curr_mapping->num_refs--;
-        if(curr_mapping->num_refs > 0){
-            curr_mapping = (pxom_mapping) curr_mapping->lhead.next;
+        if (curr_mapping->num_refs > 0)
+        {
+            curr_mapping = (pxom_mapping)curr_mapping->lhead.next;
             continue;
         }
         release_mapping(curr_mapping);
         last_mapping = curr_mapping;
-        curr_mapping = (pxom_mapping) curr_mapping->lhead.next;
+        curr_mapping = (pxom_mapping)curr_mapping->lhead.next;
         kfree(last_mapping);
-    } 
+    }
     return 0;
 }
 
-static int verify_offset(pxom_process_entry curr_entry, loff_t offset){
-    pxom_mapping last_mapping = (pxom_mapping) curr_entry->mappings.prev;
-
-    if((void*)last_mapping == &(curr_entry->mappings))
-        return 1;
-    
-    if(offset < last_mapping->mmap_offset + (last_mapping->num_pages * PAGE_SIZE))
-        return 0;
-    
-    return 1;
-}
-
-
-static pxom_mapping get_new_mapping(struct file * f, struct vm_area_struct * vma, pxom_process_entry curr_entry) {
+static pxom_mapping get_new_mapping(struct vm_area_struct *vma, pxom_process_entry curr_entry)
+{
     unsigned long size = (vma->vm_end - vma->vm_start);
-    void* newmem = NULL;
+    void *newmem = NULL;
     unsigned int i;
     int status;
     pxom_mapping new_mapping = NULL;
@@ -120,49 +112,47 @@ static pxom_mapping get_new_mapping(struct file * f, struct vm_area_struct * vma
 
     // Must be page-aligned
     if (size % PAGE_SIZE || vma->vm_start % PAGE_SIZE || !size)
+    {
         return NULL;
-    
-    if(!verify_offset(curr_entry, f->f_pos))
-        return NULL;
+    }
 
     new_mapping = kmalloc(sizeof(*new_mapping), GFP_KERNEL);
     if (!new_mapping)
         return NULL;
-    
-    newmem = (void*) __get_free_pages(GFP_KERNEL, get_order(size));
-    if(!newmem || (ssize_t) newmem == -1)
+
+    newmem = (void *)__get_free_pages(GFP_KERNEL, get_order(size));
+    if (!newmem || (ssize_t)newmem == -1)
         goto fail;
 
     // Set PG_reserved bit to prevent swapping
-    for(i = 0; i < size; i += PAGE_SIZE)
+    for (i = 0; i < size; i += PAGE_SIZE)
         SetPageReserved(virt_to_page(newmem + i));
 
     memset(newmem, 0x0, PAGE_SIZE * (1 << get_order(size)));
 
     status = remap_pfn_range(vma, vma->vm_start, page_to_pfn(virt_to_page(newmem)), size, vma->vm_page_prot);
 
-    if(status < 0)
+    if (status < 0)
         goto fail;
-    
-    *new_mapping = (xom_mapping) {
-        .mmap_offset = f->f_pos,
+
+    *new_mapping = (xom_mapping){
         .num_pages = size / PAGE_SIZE,
         .num_refs = 1,
-        .kaddr = (unsigned long) newmem,
-        .uaddr = vma->vm_start
-    };
+        .kaddr = (unsigned long)newmem,
+        .uaddr = vma->vm_start};
 
     return new_mapping;
 
 fail:
-    if(curr_entry)
+    if (curr_entry)
         kfree(curr_entry);
-    if(newmem)
+    if (newmem)
         __free_pages(virt_to_page(newmem), get_order(size));
     return NULL;
 }
 
-int	xom_open(struct inode *, struct file *) {
+int xom_open(struct inode *, struct file *)
+{
     pxom_process_entry new_entry = kmalloc(sizeof(*new_entry), GFP_KERNEL);
     new_entry->pid = current->pid;
     INIT_LIST_HEAD(&(new_entry->mappings));
@@ -170,7 +160,8 @@ int	xom_open(struct inode *, struct file *) {
     return 0;
 }
 
-int	xom_release(struct inode *, struct file *){
+int xom_release(struct inode *, struct file *)
+{
     int status;
     pxom_process_entry curr_entry = get_process_entry();
 
@@ -180,54 +171,58 @@ int	xom_release(struct inode *, struct file *){
     return status;
 }
 
-int	xom_mmap(struct file * f, struct vm_area_struct * vma){
+int xom_mmap(struct file *f, struct vm_area_struct *vma)
+{
     pxom_process_entry curr_entry = get_process_entry();
-    pxom_mapping new_mapping = get_new_mapping(f, vma, curr_entry);
+    pxom_mapping new_mapping = get_new_mapping(vma, curr_entry);
 
-    if(!new_mapping)
+    if (!new_mapping)
         return -EINVAL;
 
     list_add(&(new_mapping->lhead), &(curr_entry->mappings));
     return 0;
 }
 
-ssize_t	xom_read(struct file * f, char __user * user_mem, size_t len, loff_t * offset){
+ssize_t xom_read(struct file *f, char __user *user_mem, size_t len, loff_t *offset)
+{
     size_t len_reqired = sizeof(READ_HEADER_STRING), index, clen;
-    char* dstring;
+    char *dstring;
     pxom_process_entry curr_entry = get_process_entry();
     pxom_mapping curr_mapping;
-    struct vm_area_struct* vma;
+    struct vm_area_struct *vma;
 
-    curr_mapping = (pxom_mapping) curr_entry->mappings.next;
-    while ((void*)curr_mapping != &(curr_entry->mappings)){
+    curr_mapping = (pxom_mapping)curr_entry->mappings.next;
+    while ((void *)curr_mapping != &(curr_entry->mappings))
+    {
         len_reqired += MAPPING_LINE_SIZE;
-        curr_mapping = (pxom_mapping) curr_mapping->lhead.next;
+        curr_mapping = (pxom_mapping)curr_mapping->lhead.next;
     }
 
-    if(*offset >= len_reqired)
+    if (*offset >= len_reqired)
         return 0;
 
     dstring = kvmalloc(len_reqired, GFP_KERNEL);
-    if(!dstring)
+    if (!dstring)
         return -ENOMEM;
-    
+
     memcpy(dstring, READ_HEADER_STRING, sizeof(READ_HEADER_STRING));
-    curr_mapping = (pxom_mapping) curr_entry->mappings.next;
+    curr_mapping = (pxom_mapping)curr_entry->mappings.next;
     index = sizeof(READ_HEADER_STRING) - 1;
-    while ((void*)curr_mapping != &(curr_entry->mappings) && index < len_reqired){
+    while ((void *)curr_mapping != &(curr_entry->mappings) && index < len_reqired)
+    {
         vma = find_vma(current->mm, curr_mapping->uaddr);
-        if(!vma) {
-            curr_mapping = (pxom_mapping) curr_mapping->lhead.next;
+        if (!vma)
+        {
+            curr_mapping = (pxom_mapping)curr_mapping->lhead.next;
             continue;
         }
-        index += snprintf(dstring + index, len_reqired - index, "%16lx, %16lx, %16lx\n", 
-                    vma->vm_start, vma->vm_end - vma->vm_start, (unsigned long) curr_mapping->mmap_offset);
-        curr_mapping = (pxom_mapping) curr_mapping->lhead.next;
+        index += snprintf(dstring + index, len_reqired - index, "%16lx, %16lx\n",
+                          vma->vm_start, vma->vm_end - vma->vm_start);
+        curr_mapping = (pxom_mapping)curr_mapping->lhead.next;
     }
 
-    
-    clen = MIN(len_reqired - (unsigned long) *offset, len);
-    if(copy_to_user(user_mem, dstring + *offset, clen))
+    clen = MIN(len_reqired - (unsigned long)*offset, len);
+    if (copy_to_user(user_mem, dstring + *offset, clen))
         clen = 0;
     *offset += clen;
     kvfree(dstring);
@@ -235,7 +230,8 @@ ssize_t	xom_read(struct file * f, char __user * user_mem, size_t len, loff_t * o
     return clen;
 }
 
-ssize_t	xom_write(struct file * _f, const char __user * _user_mem, size_t _len, loff_t * _offset){
+ssize_t xom_write(struct file *_f, const char __user *_user_mem, size_t _len, loff_t *_offset)
+{
     return -EINVAL;
 }
 
@@ -247,35 +243,36 @@ const static struct proc_ops file_ops = {
     .proc_mmap = xom_mmap,
 };
 
-
 static int __init
-MODXOM_init(void) {
+MODXOM_init(void)
+{
     struct proc_dir_entry *entry;
     printk(KERN_INFO
-    "[MODXOM] Hello World!\n");
+           "[MODXOM] Hello World!\n");
     entry = proc_create(MODXOM_PROC_FILE_NAME, 0666, NULL, &file_ops);
     printk(KERN_INFO
-    "[MODXOM] MODXOM Kernel Module loaded!\n");
+           "[MODXOM] MODXOM Kernel Module loaded!\n");
     return 0;
 }
 
 static void __exit
-MODXOM_exit(void) {
-    pxom_process_entry curr_entry = (pxom_process_entry) xom_entries.next, last_entry;
-    while((void*) curr_entry != &xom_entries){
+MODXOM_exit(void)
+{
+    pxom_process_entry curr_entry = (pxom_process_entry)xom_entries.next, last_entry;
+    while ((void *)curr_entry != &xom_entries)
+    {
         release_process(curr_entry);
         last_entry = curr_entry;
-        curr_entry = (pxom_process_entry) curr_entry->lhead.next;
+        curr_entry = (pxom_process_entry)curr_entry->lhead.next;
         kfree(last_entry);
     }
 
     remove_proc_entry(MODXOM_PROC_FILE_NAME, NULL);
     printk(KERN_INFO
-    "[MODXOM] MODXOM Kernel Module unloaded. Goodbye!\n");
+           "[MODXOM] MODXOM Kernel Module unloaded. Goodbye!\n");
 }
 
 module_init(MODXOM_init);
 module_exit(MODXOM_exit);
 
 MODULE_LICENSE("MIT");
-
