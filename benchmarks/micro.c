e95d92ad121bfe6bc88a1fbb35a704688dd3af81
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2023-11-28 21:03:09 +0100

    Remove clflush benchmark

--- benchmarks/micro.c
+++ benchmarks/micro.c
@@ -13,52 +13,6 @@
 #define PAGE_SIZE 0x1000
 #endif
 
-benchmark(nop_slide_clflush) {
-    uint64_t timer;
-    const static unsigned num_repetitions = 1000, num_rounds = 100;
-    unsigned i, j;
-    char *nop_slide = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    struct xombuf* nop_slide_xom = xom_alloc_pages(PAGE_SIZE);
-    void (*nop_slide_fun)(void) = ((void (*)(void))nop_slide);
-    void (*nop_slide_xom_fun)(void);
-    unsigned times[num_repetitions];
-
-
-    memset(nop_slide, NOP, PAGE_SIZE);
-    nop_slide[PAGE_SIZE - 1] = RET;
-    xom_write(nop_slide_xom, nop_slide, PAGE_SIZE);
-    nop_slide_xom_fun = (void (*)(void)) xom_lock(nop_slide_xom);
-
-    fprintf(fp, "nop_slide_times_noxom = [\n");
-    for(j = 0; j < num_rounds; j++) {
-        flush_all(nop_slide, PAGE_SIZE);
-        for(i = 0; i< num_repetitions; i++) {
-            START_TIMER;
-            nop_slide_fun();
-            TIME_ELAPSED(timer);
-            times[i] = timer;
-        }
-        write_list(fp, times, sizeof(times)/sizeof(*times), ',');
-        sched_yield();
-    }
-    fprintf(fp, "]\n\nnop_slide_times_xom = [");
-    fflush(fp);
-    for(j = 0; j < num_rounds; j++) {
-        flush_all(nop_slide_xom_fun, PAGE_SIZE);
-        for(i = 0; i< num_repetitions; i++){
-            START_TIMER;
-            nop_slide_xom_fun();
-            TIME_ELAPSED(timer);
-            times[i] = timer;
-        }
-        write_list(fp, times, countof(times), ',');
-        sched_yield();
-    }
-    fprintf(fp, "]\n");
-    munmap(nop_slide, PAGE_SIZE);
-    xom_free(nop_slide_xom);
-    return 0;
-}
 
 benchmark(nop_slide) {
     uint64_t timer;
@@ -118,7 +72,6 @@ int main(int argc, char* argv[]){
         return 1;
 
     run_benchmark(nop_slide);
-    run_benchmark(nop_slide_clflush);
 
     exit_utils();
     return 0;
